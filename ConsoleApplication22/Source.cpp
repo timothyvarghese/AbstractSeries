23cee0fdce7561477ca760f5a2bd6f6f8feca0f6
timothyvarghese <timothyvarghese@g.ucla.edu>
2018-03-16 15:44:24 -0700

    added sums of cousin primes and twin primes

--- ConsoleApplication22/Source.cpp
+++ ConsoleApplication22/Source.cpp
@@ -6,27 +6,43 @@
 class SpecialintIterator {
 public:
 	SpecialintIterator(int start) {position = start;}
-	void next(bool(*specialintfunction) (int)) // Jumps to the next special int 
+	void next(bool(*specialintfunction) (int)) // Jumps to the next special int
+		// Based on specialintfunction which takes an integer as a parameter
+		// and returns true if the number is a special number
 	{
-		position++; 
+		position++; // Go to the first position after the last special number
 		while (specialintfunction(position) != true) // While not special
 			position++; 
 	}; 
 	int getSpecialInt() { return position; } // Return the current special integer
 	void printSpecialInt() { std::cout << "The special integer is " << position << "\n"; }
 private:
-	int position;
+	int position; // The current or last position of the last integer 
 }; 
 
 
 bool isPrime(int n) // This is a function that checks if n is a prime.
 {
-	for (int i = 2; i < n; i++)
-		if (n % i == 0)
+	for (int i = 2; i < n; i++) // Go through all the integers less than n
+		if (n % i == 0) // if n divides evenly it is not a prime
 			return false;
-	return true; 
+	return true; // if no integer divides return true
 }
 
+bool isTwinPrime(int n) // This is a function that checks if n is a twin prime.
+{
+	// n is a twin prime if n and n+2 are both prime
+	if (isPrime(n) && isPrime(n+2)) 
+			return true;
+	else return false;
+}
+bool isCousinPrime(int n) // This is a function that checks if n is a cousin prime.
+{
+	// n is a cousin prime if n and n+4 are both prime
+	if (isPrime(n) && isPrime(n + 4))
+		return true;
+	else return false; // 
+}
 bool isPerfect(int n) // This is a function that checks if n is a perfect.
 // A number is perfect when that number is the sum of all its factors
 // Hence, to check if a number is perfect, we have to find all the factors and add
@@ -65,9 +81,18 @@ int main()
 
 	// Rewrite code above with abstract iterator
 
-	SpecialintIterator p(2); // Create a prime iterator staring at 2
-	sum = 0;
+	SpecialintIterator p(2); // Create a prime iterator starting at 2
+	sum = 0; // Sum is initialized to 0
 	for (int i = 0; i < max; p.next(isPrime)) {
+		sum = sum + p.getSpecialInt();
+		i++;
+		p.printSpecialInt();
+	}
+	std::cout << "The sum of the first  " << max << " special numbers is " << sum << "\n";
+
+	SpecialintIterator p(2); // Create a prime iterator starting at 2
+	sum = 0; // Sum is initialized to 0
+	for (int i = 0; i < max; p.next(isTwinprime)) {
 		sum = sum + p.getSpecialInt();
 		i++;
 		p.printSpecialInt();
