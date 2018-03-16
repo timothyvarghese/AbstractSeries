3e2876a29ee7a2efd48b132c253508d32cc11fe1
timothyvarghese <timothyvarghese@g.ucla.edu>
2018-03-16 11:04:42 -0700

    abstract code with iterators first pass

--- ConsoleApplication22/Source.cpp
+++ ConsoleApplication22/Source.cpp
@@ -1,5 +1,24 @@
 # include <iostream>
 
+// Now we abstract the code using an iterator and function pointers to abstract across
+// isPrime and isPerfect so we can add new functions in the future without cutting and pasting
+
+class SpecialintIterator {
+public:
+	SpecialintIterator(int start) {position = start;}
+	void next(bool(*specialintfunction) (int)) // Jumps to the next special int 
+	{
+		position++; 
+		while (specialintfunction(position) != true) // While not special
+			position++; 
+	}; 
+	int getSpecialInt() { return position; } // Return the current special integer
+	void printSpecialInt() { std::cout << "The special integer is " << position << "\n"; }
+private:
+	int position;
+}; 
+
+
 bool isPrime(int n) // This is a function that checks if n is a prime.
 {
 	for (int i = 2; i < n; i++)
@@ -24,7 +43,7 @@ bool isPerfect(int n) // This is a function that checks if n is a perfect.
 int main()
 {
 	int max = 10;
-	int sum = 0; 
+	int sum = 0;
 
 	for (int i = 2; i <= max; i++) { // Goes through all integers less than max
 		if (isPrime(i) == true) { // Update if i is a prime 
@@ -32,10 +51,10 @@ int main()
 			std::cout << i << " is a prime \n";
 		}
 	}
-	std::cout << "The sum of the primes less than " << max << " is " << sum << "\n"; 
+	std::cout << "The sum of the primes less than " << max << " is " << sum << "\n";
 
-	
-	sum = 0; 
+
+	sum = 0;
 	for (int i = 2; i <= max; i++) { // Goes through all integers less than max
 		if (isPerfect(i) == true) { // Update if i is a perfect number
 			sum = sum + i;
@@ -43,7 +62,16 @@ int main()
 		}
 	}
 	std::cout << "The sum of the perfect numbers less than " << max << " is " << sum << "\n";
-	
 
+	// Rewrite code above with abstract iterator
+
+	SpecialintIterator p(2); // Create a prime iterator staring at 2
+	sum = 0;
+	for (int i = 0; i < max; p.next(isPrime)) {
+		sum = sum + p.getSpecialInt();
+		i++;
+		p.printSpecialInt();
+	}
+	std::cout << "The sum of the first  " << max << " special numbers is " << sum << "\n";
 	return 0; 
-}
+} 
