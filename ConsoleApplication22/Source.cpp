591e83e3a43a4ee1b0f21c08d7ef87c23247e7a1
timothyvarghese <timothyvarghese@g.ucla.edu>
2018-03-16 10:18:09 -0700

    added sums of perfect numbers

--- ConsoleApplication22/Source.cpp
+++ ConsoleApplication22/Source.cpp
@@ -1,11 +1,24 @@
 # include <iostream>
 
-bool isPrime(int n)
+bool isPrime(int n) // This is a function that checks if n is a prime.
 {
 	for (int i = 2; i < n; i++)
 		if (n % i == 0)
 			return false;
 	return true; 
+}
+
+bool isPerfect(int n) // This is a function that checks if n is a perfect.
+// A number is perfect when that number is the sum of all its factors
+// Hence, to check if a number is perfect, we have to find all the factors and add
+// them and check if the sum is equal to the number
+{
+	int sum = 0; 
+	for (int i = 1; i < n; i++)
+		if (n % i == 0)
+			sum= sum + i ;
+	if (sum == n) return true; 
+	else return false;
 }
 
 int main()
@@ -19,9 +32,9 @@ int main()
 			std::cout << i << " is a prime \n";
 		}
 	}
-	std::cout << "The sum of the first " << max << " primes is " << sum << "\n"; 
+	std::cout << "The sum of the primes less than " << max << " is " << sum << "\n"; 
 
-	/*
+	
 	sum = 0; 
 	for (int i = 2; i <= max; i++) { // Goes through all integers less than max
 		if (isPerfect(i) == true) { // Update if i is a perfect number
@@ -29,8 +42,8 @@ int main()
 			std::cout << i << " is a perfect number \n";
 		}
 	}
-	std::cout << "The sum of the first " << max << " perfect numbers is " << sum << "\n";
-	*/
+	std::cout << "The sum of the perfect numbers less than " << max << " is " << sum << "\n";
+	
 
 	return 0; 
 }
